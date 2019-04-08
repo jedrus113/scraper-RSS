1d923f50211e058d319695cb9d8440bea6a2c0c4
Andrzej Dabski <andrzej.dabski@ulam.io>
2019-04-08 20:22:55 +0200

    models for worker and raw data

---
+++ scrapper_rss/models.py
@@ -0,0 +1,26 @@
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class RSSTypes(BaseModel):
+    last_fetch = models.DateTimeField()
+    active = models.BooleanField(default=True)
+    type = models.CharField(max_length=100)
+    url = models.TextField()
+
+
+class RSSData(BaseModel):
+    date = models.DateField()
+    type = models.ForeignKey(
+        'RSSTypes',
+        on_delete=models.CASCADE,
+    )
+    data = JSONField()
